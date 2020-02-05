02d0fb1dc5027e250500dcc0886c4a0e80799a7e
Jeremy Ho <j3jeremy@gmail.com>
2020-02-04 16:50:39 -0800

    working

--- Test_Shock_From_Matlab.m
+++ Test_Shock_From_Matlab.m
@@ -5,6 +5,11 @@ disp('Begin Shock from Matlab');
 
 % pyversion
 % path = fileparts(which(test.py));
-system('python Stimulation_files/shock.py 5')
+shock_file = ' Stimulation_files/shock.py'
+num_shocks = 5
+terminal_input = strcat('python', ' ', shock_file, ' ', num_shocks);
+disp(terminal_input);
+
+% system('python Stimulation_files/shock.py 5')
 
 disp('Finished');
