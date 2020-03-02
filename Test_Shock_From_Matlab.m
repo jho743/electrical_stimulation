542674c6819b8cfaa02527d30d5775b49581ebb9
Alan Lee <alan.s.lee@berkeley.edu>
2020-03-02 12:48:16 -0800

    ready to test/ write code to incorporate shocking

--- Test_Shock_From_Matlab.m
+++ Test_Shock_From_Matlab.m
@@ -5,11 +5,11 @@ disp('Begin Shock from Matlab');
 
 % pyversion
 % path = fileparts(which(test.py));
-shock_file = ' Stimulation_files/shock.py'
-num_shocks = 5
+shock_file = ' Stimulation_files/shock.py';
+num_shocks = 5;
 terminal_input = strcat('python', ' ', shock_file, ' ', num_shocks);
 disp(terminal_input);
 
-% system('python Stimulation_files/shock.py 5')
+system('python Stimulation_files/shock.py 5')
 
 disp('Finished');
